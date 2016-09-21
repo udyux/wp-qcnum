2f998db61632e37d1ad751192b3db5c5614aa150
Nicolas Udy <dev@udy.io>
2016-09-21 01:20:52 -0400

    Add repeater to about page

--- layouts/about.php
+++ layouts/about.php
@@ -24,8 +24,9 @@
   <header class="about__header">
     <div class="about__background" style="background-image:url(<?= $header; ?>);"></div>
 
-    <h1 class="about__title about__title--main"><?= $beginning['date']; ?></h1>
+    <h1 class="about__title"><?= $beginning['date']; ?></h1>
     <div class="about__circle"></div>
+
     <div class="about__box">
       <h2 class="about__subtitle"><?= $beginning['title']; ?></h2>
       <p class="about__text about__text--beginning"><?= $beginning['text']; ?></p>
@@ -33,7 +34,7 @@
   </header>
 
   <section class="about__section">
-    <h2 class="about__title"><?= $waq['title']; ?></h2>
+    <h2 class="about__title about__title--waq"><?= $waq['title']; ?></h2>
     <p class="about__text about__text--waq"><?= $waq['text']; ?></p>
 
     <div class="row">
@@ -46,30 +47,53 @@
         <div class="col__item infographic">
           <h3 class="infographic__title"><?= $waq['volunteer']; ?> bénévoles</h3>
           <div class="infographic__image infographic__image--volunteers"></div>
-          <svg class="infographic__image"><use xlink:href="#about__volunteers"></use></svg>
         </div>
       </div>
 
       <div class="row__item col">
         <div class="col__item infographic infographic--small">
           <div class="infographic__image infographic__image--database"></div>
-          <h3 class="infographic__title"><?= $waq['data']; ?> giga<br>bytes</h3>
+          <h3 class="infographic__title"><?= $waq['data']; ?> giga bytes</h3>
         </div>
 
         <div class="col__item infographic infographic--small">
           <div class="infographic__image infographic__image--money"></div>
-          <h3 class="infographic__title">budget<br>de <?= $waq['budget']; ?></h3>
+          <h3 class="infographic__title">budget de <?= $waq['budget']; ?></h3>
         </div>
 
         <div class="col__item infographic infographic--small">
           <div class="infographic__image infographic__image--coffee"></div>
-          <h3 class="infographic__title"><?= $waq['coffee']; ?><br>cafés</h3>
+          <h3 class="infographic__title"><?= $waq['coffee']; ?> cafés</h3>
         </div>
       </div>
     </div>
   </section>
 
-  <section class="about__section">
+  <div>
 
-  </section>
+    <?
+      if ( have_rows('milestones') ) : while ( have_rows('milestones') ) : the_row();
+
+      $date  = get_sub_field('date');
+      $title = get_sub_field('title');
+      $text  = get_sub_field('text');
+      $image = get_sub_field('background');
+      $noImg = empty($image); ?>
+
+      <section class="milestone<? if ($noImg) echo ' milestone--noImg' ?>">
+        <div class="milestone__background" style="background-image:url(<?= $image; ?>);"></div>
+
+        <h1 class="milestone__title"><?= $date; ?></h1>
+        <div class="milestone__circle"></div>
+
+        <div class="milestone__box">
+          <h2 class="milestone__subtitle"><?= $title; ?></h2>
+          <p class="milestone__text"><?= $text; ?></p>
+        </div>
+      </section>
+
+    <? endwhile; endif; ?>
+
+  </div>
+
 </article>
