11e88e913b4a0711dcb51b531645de34bc1eeaf0
Nicolas Udy <dev@udy.io>
2016-09-28 11:17:16 -0400

    Temp commit

--- layouts/partials/main-nav.php
+++ layouts/partials/main-nav.php
@@ -22,7 +22,15 @@
 
     <? endwhile; endif; ?>
 
-    <? _udyux_get_partial('form', 'search'); ?>
+    <!-- <? _udyux_get_partial('form', 'search'); ?> -->
+    <form id="search-form" role="search" method="GET" class="search" action="<?= home_url( '/' ); ?>">
+      <input id="search-input" class="search__input" type="text" placeholder="Rechercher ..." value="<?= get_search_query(); ?>" name="s" autocomplete="off" required>
+      <label class="search__label" for="search-input">
+        <svg class="search__icon"><use xlink:href="#search"></use></svg>
+      </label>
+      <input id="search-submit" class="search__submit" type="submit" value="">
+    </form>
+
 
   </menu>
 </nav>
